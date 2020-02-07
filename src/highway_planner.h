fdcd05e792510b3b7ab961f5600fb2fa8d66dce2
Daniel Martinez <danmartinez@gatech.edu>
2020-02-06 20:33:16 -0600

    added basic fsm states to highway planner class

--- src/highway_planner.h
+++ src/highway_planner.h
@@ -2,8 +2,8 @@
 #define HIGHWAY_PLANNER_CLASS
 
 #include "behavior_planner.h" // behavior planner
-#include "spline.h"    // spline library
-#include "helpers.h"   // helper functions
+#include "spline.h"           // spline library
+#include "helpers.h"          // helper functions
 
 class HighwayPlanner
 {
@@ -45,11 +45,18 @@ public:
         double y;
     };
 
+    enum class State{
+        KEEP_LANE, PLCL, PLCR, LCL, LCR
+    };
+
 private:
     /* data */
 
     HighwayPlanner::Pose pose;
-    // TODO: state machine
+    
+    // TODO: simple state machine
+ 
+    State state = State::KEEP_LANE;
 
     // trajs
     tk::spline prev_spline;
@@ -83,65 +90,87 @@ void HighwayPlanner::SetPose(double x, double y, double s, double d, double yaw,
 
 bool HighwayPlanner::Plan()
 {
-    double pos_x;
-    double pos_y;
-    double angle;
-    int path_size = prev_path.size();
-    next_path.clear();
-
-    for (int i = 0; i < path_size; ++i)
-    {
-        Point pt;
-        pt.x = prev_path[i].x;
-        pt.y = prev_path[i].y;
-        next_path.push_back(pt);
+    switch(state){
+        case State::KEEP_LANE:
+            // blah
+            break;
+        case State::PLCL:
+            // blah
+            break;
+        case State::PLCR:
+            // blah
+            break;
+        case State::LCL:
+            // blah
+            break;
+        case State::LCR:
+            // blah
+            break;
+        default:
+            // blah
     }
+    // double pos_x;
+    // double pos_y;
+    // double angle;
+    // int path_size = prev_path.size();
+    // next_path.clear();
 
-    if (path_size == 0)
-    {
-        pos_x = pose.x;
-        pos_y = pose.y;
-        angle = deg2rad(pose.yaw);
-    }
-    else
-    {
-        pos_x = prev_path[path_size - 1].x;
-        pos_y = prev_path[path_size - 1].y;
+    // for (int i = 0; i < path_size; ++i)
+    // {
+    //     Point pt;
+    //     pt.x = prev_path[i].x;
+    //     pt.y = prev_path[i].y;
+    //     next_path.push_back(pt);
+    // }
 
-        double pos_x2 = prev_path[path_size - 2].x;
-        double pos_y2 = prev_path[path_size - 2].y;
-        angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
-    }
+    // if (path_size == 0)
+    // {
+    //     pos_x = pose.x;
+    //     pos_y = pose.y;
+    //     angle = deg2rad(pose.yaw);
+    // }
+    // else
+    // {
+    //     pos_x = prev_path[path_size - 1].x;
+    //     pos_y = prev_path[path_size - 1].y;
 
-    double dist_inc = 0.5;
-    for (int i = 0; i < 50 - path_size; ++i)
-    {
-        Point pt;
-        pt.x = pos_x + (dist_inc)*cos(angle + (i + 1) * (pi() / 100));
-        pt.y = pos_y + (dist_inc)*sin(angle + (i + 1) * (pi() / 100));
-        next_path.push_back(pt);
-        pos_x += (dist_inc)*cos(angle + (i + 1) * (pi() / 100));
-        pos_y += (dist_inc)*sin(angle + (i + 1) * (pi() / 100));
-    }
-    prev_path = next_path;
-    return true;
+    //     double pos_x2 = prev_path[path_size - 2].x;
+    //     double pos_y2 = prev_path[path_size - 2].y;
+    //     angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
+    // }
+
+    // double dist_inc = 0.5;
+    // for (int i = 0; i < 50 - path_size; ++i)
+    // {
+    //     Point pt;
+    //     pt.x = pos_x + (dist_inc)*cos(angle + (i + 1) * (pi() / 100));
+    //     pt.y = pos_y + (dist_inc)*sin(angle + (i + 1) * (pi() / 100));
+    //     next_path.push_back(pt);
+    //     pos_x += (dist_inc)*cos(angle + (i + 1) * (pi() / 100));
+    //     pos_y += (dist_inc)*sin(angle + (i + 1) * (pi() / 100));
+    // }
+    // prev_path = next_path;
+    // return true;
 };
 
 std::pair<std::vector<double>, std::vector<double>> HighwayPlanner::GetPlannedPath()
 {
     vector<double> next_x_vals;
     vector<double> next_y_vals;
-    for(auto pt:next_path){
+    for (auto pt : next_path)
+    {
         next_x_vals.push_back(pt.x);
         next_y_vals.push_back(pt.y);
     }
     return std::make_pair(next_x_vals, next_y_vals);
 }
 
-void HighwayPlanner::SetPrevPath(std::vector<double> last_path_x, std::vector<double> last_path_y){
+void HighwayPlanner::SetPrevPath(std::vector<double> last_path_x, std::vector<double> last_path_y)
+{
     this->prev_path.clear();
     int path_size = last_path_x.size();
-    for (int i = 0; i<path_size;i++){
+    for (int i = 0; i < path_size; i++)
+    {
         Point pt;
         pt.x = last_path_x[i];
         pt.y = last_path_y[i];
